8ed5c9911e3d56113d5733f30d4daeac81a15e08
hemanthghs <ghshemanth@gmail.com>
2023-09-09 19:29:57 +0530

    initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TopNav from "./components/TopNav";
+import Content from "./components/Content";
+
+const Home = () => {
+  return (
+    <>
+      <TopNav />
+      <Content />
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/Content.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SideNav from "./SideNav";
+import MainContent from "./MainContent";
+
+const Content = () => {
+  return (
+    <>
+      <SideNav />
+      <MainContent />
+    </>
+  );
+};
+
+export default Content;
