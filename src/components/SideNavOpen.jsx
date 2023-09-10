78f115f7cc5cb41f0d515dff41b4eb2fdd511d9e
hemanthghs <ghshemanth@gmail.com>
2023-09-11 00:01:43 +0530

    Add fav chains

--- src/components/SideNavOpen.jsx
+++ src/components/SideNavOpen.jsx
@@ -15,6 +15,7 @@ import {
   ApplicationsIcon,
   ReleaseNotesIcon,
   ArrowDown,
+  StarIcon,
 } from "../assets/Icons";
 
 const SideNavOpen = () => {
@@ -25,15 +26,40 @@ const SideNavOpen = () => {
   return (
     <div>
       <div className="p-2 pt-3">
-        <div className="mb-1 flex cursor-pointer items-center justify-between px-3 py-[13px] text-[#8d98a5] transition duration-150 ease-out hover:rounded-md hover:bg-[#1c1d2a] hover:text-[#dde0e4] hover:ease-in">
-          <div className="text-[14px] font-semibold">EXPLORER</div>
-          <div
-            onClick={() => {
-              setEOpen((x) => !x);
-            }}
-          >
-            {eOpen ? <ArrowUp /> : <ArrowDown />}
+        <div className="mb-1 mt-1 flex cursor-pointer flex-col items-center px-1 pt-[13px] pb-[10px] text-[#8d98a5] transition duration-200 ease-out hover:rounded-md hover:bg-[#1c1d2a] hover:ease-in">
+          <div className="flex w-full items-center justify-between px-2 hover:text-[#dde0e4]">
+            <div className="text-[14px] font-semibold">EXPLORER</div>
+            <div
+              onClick={() => {
+                setEOpen((x) => !x);
+              }}
+            >
+              {eOpen ? <ArrowUp /> : <ArrowDown />}
+            </div>
           </div>
+          {eOpen && (
+            <div className="w-full mt-3">
+              <NetworkItem
+                logo={"https://www.mintscan.io/assets/chains/png_32/cosmos.png"}
+                name={"COSMOS"}
+                badge={null}
+              />
+              <NetworkItem
+                logo={
+                  "https://www.mintscan.io/assets/chains/png_32/osmosis.png"
+                }
+                name={"OSMOSIS"}
+                badge={null}
+              />
+              <NetworkItem
+                logo={
+                  "https://www.mintscan.io/assets/chains/png_32/archway.png"
+                }
+                name={"ARCHWAY"}
+                badge={null}
+              />
+            </div>
+          )}
         </div>
         <div className="border-b-[1px] border-[#282a32]"></div>
         <div className="mb-1 mt-1 flex cursor-pointer flex-col items-center px-1 pt-[13px] pb-[10px] text-[#8d98a5] transition duration-200 ease-out hover:rounded-md hover:bg-[#1c1d2a] hover:ease-in">
@@ -97,8 +123,14 @@ const SideNavOpen = () => {
             <div className="w-full mt-3">
               <CustomSideBarItem Icon={<NoticeIcon />} name={"Notice"} />
               <CustomSideBarItem Icon={<BlogIcon />} name={"Blog"} />
-              <CustomSideBarItem Icon={<ApplicationsIcon />} name={"Applications"} />
-              <CustomSideBarItem Icon={<ReleaseNotesIcon />} name={"Release Note"} />
+              <CustomSideBarItem
+                Icon={<ApplicationsIcon />}
+                name={"Applications"}
+              />
+              <CustomSideBarItem
+                Icon={<ReleaseNotesIcon />}
+                name={"Release Note"}
+              />
             </div>
           )}
         </div>
@@ -116,6 +148,21 @@ const CustomSideBarItem = ({ Icon, name }) => {
       <div className="ml-[9px] text-[14px] font-semibold text-[#dde0e4]">
         {name}
       </div>
+    </div>
+  );
+};
+
+const NetworkItem = ({ logo, name, badge = null }) => {
+  return (
+    <div className="hover:bg-[#252632] rounded-md py-2 px-3 cursor-pointer flex justify-between items-center">
+      <div className="flex items-center">
+        <div className="flex items-center">
+          <div className="mt-[-3px] mr-2"><StarIcon width={13} height={13} fill={"#5e8fb3"}/></div>
+          <img className="h-[18px] w-[18px]" src={logo} alt="" />
+        </div>
+        <div className="ml-3 text-[#e8eaed] text-[14px] font-semibold">{name}</div>
+      </div>
+      <div>{badge}</div>
     </div>
   );
 };
