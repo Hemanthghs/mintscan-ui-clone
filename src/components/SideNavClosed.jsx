7da7f722f9846c45fa8ff45337b4e0a8e1b7caf7
hemanthghs <ghshemanth@gmail.com>
2023-09-11 01:44:51 +0530

    Add logos sidenavClosed

--- src/components/SideNavClosed.jsx
+++ src/components/SideNavClosed.jsx
@@ -17,10 +17,22 @@ import {
 } from "../assets/Icons";
 
 const SideNavClosed = () => {
+  const chains = [
+    "https://www.mintscan.io/assets/chains/png_32/cosmos.png",
+    "https://www.mintscan.io/assets/chains/png_32/osmosis.png",
+    "https://www.mintscan.io/assets/chains/png_32/archway.png",
+  ];
   return (
     <div>
       <div className="p-2 pt-3">
-        <div className="mb-1 flex cursor-pointer items-center justify-between px-3 py-[8px] text-[#8d98a5] transition duration-150 ease-out hover:rounded-md hover:bg-[#1c1d2a] hover:text-[#dde0e4] hover:ease-in">
+        <div className="mb-1 mt-1 flex cursor-pointer flex-col items-center px-1 py-[13px] text-[#8d98a5] transition duration-200 ease-out hover:rounded-md hover:bg-[#1c1d2a] hover:ease-in">
+          {chains.map((logo, index) => (
+            <div className="flex w-full items-center rounded-md px-2 py-2 hover:bg-[#252632]">
+              <div className="h-[18px] w-[18px]">
+                <img className="h-[18px] w-[18px]" src={logo} alt="" />
+              </div>
+            </div>
+          ))}
         </div>
         <div className="border-b-[1px] border-[#282a32]"></div>
         <div className="mb-1 mt-1 flex cursor-pointer flex-col items-center px-1 py-[13px] text-[#8d98a5] transition duration-200 ease-out hover:rounded-md hover:bg-[#1c1d2a] hover:ease-in">
